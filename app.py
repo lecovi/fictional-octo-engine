a95749931de4ee8a55765dae446b95f4acffabf2
Marcos Damián Mesmer y Rosset <marcosdmyr@gmail.com>
2021-11-22 15:01:07 -0300

    feat: add redis service

--- app.py
+++ app.py
@@ -1,16 +1,36 @@
 import os
+import redis
 from dotenv import load_dotenv
 from flask import Flask
+from flask_redis import FlaskRedis
 
 
 load_dotenv()
-VERSION = os.environ["VERSION"]
+APP_VERSION = os.environ["APP_VERSION"]
 APP_NAME = os.environ["APP_NAME"]
 
+
+r = redis.Redis(host="redis", port=6379, db=0)
+r.set("app_name", APP_NAME)
+r.set("app_version", APP_VERSION)
+r.set("foo", "f00")
+r.set("bar", "b4r")
+r.set("baz", "b4z")
+
+
 app = Flask(__name__)
-app.logger.debug(f"Running {APP_NAME} v{VERSION}")
+app.config["REDIS_URL"] =  "redis://redis:6379/0"
+
+redis_client = FlaskRedis(app)
+
+app.logger.debug(f"Running {APP_NAME} v{APP_VERSION}")
 
 
 @app.route('/')
 def hello_world():
-    return f'Hello, World! from {APP_NAME} v{VERSION}'
+    name = redis_client.get("app_name").decode()
+    version = redis_client.get("app_version").decode()
+
+    return (
+        f'Hello, World! from {name} v{version}'
+    )
